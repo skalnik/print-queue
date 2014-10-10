888639e4accd0236496b7ce46458a4468596502d
Mike Skalnik <mike.skalnik@gmail.com>
2014-10-10 14:45:24 -0700

    Fix user facing queue additions :white_check_mark:

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var clearQueue = require('../lib/clearQueue.js');
 var getQueue = require('../lib/getQueue.js');
 
 router.get('/', function(req, res) {
   db = req.db;
   getQueue(db, res, function(queue) {
+    res.render('index', { queue: queue });
+  });
+});
+
+router.post('/', function(req, res) {
+  getQueue(req.db, res, function(queue) {
     if(req.body.queue !== undefined) {
       toQueue = req.body.queue;
       queue.push(toQueue);
@@ -13,8 +18,9 @@ router.get('/', function(req, res) {
         if(err) res.render('error', { error: err });
         else res.render('index', { queue: queue });
       });
+    } else {
+      res.render('index', { queue: queue });
     }
-    else res.render('index', { queue: queue });
   });
 });
 
