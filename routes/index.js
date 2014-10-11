1f3c2ffa86721130a9069c454062730af9ecf7ca
Mike Skalnik <mike.skalnik@gmail.com>
2014-10-10 22:22:10 -0700

    Make a queue item constructor :construction:

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var getQueue = require('../lib/getQueue.js');
-var validate = require('../lib/validate.js');
+var newItem = require('../lib/newQueueItem.js');
 
 router.get('/', function(req, res) {
   db = req.db;
@@ -15,26 +15,22 @@ router.post('/', function(req, res) {
   var locals = { queue: [], errors: [] };
   getQueue(db, res, function(queue) {
     locals.queue = queue;
-    var queueItem = req.body.queue;
-    if(queueItem !== undefined) {
-      queueItemValid = validate(queueItem);
-      if(queueItemValid.valid) {
-        queue.push(queueItem);
-        db.put('queue', queue, function(err) {
-          if(err) {
-            locals.errors.push("Could not update database!");
-            locals.errors.push(err);
-          }
-        });
-      }
-      else {
-        for(var i in queueItemValid.msgs) {
-          msg = queueItemValid.msgs[i];
-          locals.errors.push(msg);
+    var queueItem = newItem(req.body.queue);
+    if(queueItem && queueItem.valid) {
+      queue.push(queueItem);
+      db.put('queue', queue, function(err) {
+        if(err) {
+          locals.errors.push("Could not update database!");
+          locals.errors.push(err);
         }
+      });
+    }
+    else {
+      for(var i in queueItem.errors) {
+        msg = queueItem.errors[i];
+        locals.errors.push(msg);
       }
     }
-    else { locals.errors.push('Nothing submitted!') }
     res.render('index', locals);
   });
 });
