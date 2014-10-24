9d573f409823d4096e75027282ff26275dfbebca
Mike Skalnik <mike.skalnik@gmail.com>
2014-10-24 14:40:21 -0700

    WIP of removing an individual item :construction:

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var auth = require('basic-auth');
 var clearQueue = require('../lib/clearQueue.js');
 var getQueue = require('../lib/getQueue.js');
+var removeItem = require('../lib/removeItem.js');
 
 router.all('*', function(req, res, next) {
   user = auth(req);
@@ -23,6 +24,13 @@ router.get('/', function(req, res) {
 
 router.delete('/queue', function(req, res) {
   queue = clearQueue(req.db, res);
+  res.redirect('/admin');
+});
+
+router.delete('/queue/:id', function(req, res) {
+  console.log("Cockass: ", req.params.id)
+  var id = req.params.id;
+  var item = removeItem(req.db, res, id);
   res.redirect('/admin');
 });
 
