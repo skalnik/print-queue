75d27ebc959d0bec8d5d89ffa77fed61d5732a2b
Suz Hinton <suz.hinton@gmail.com>
2014-12-02 19:13:41 -0800

    rejigged browserify file into two, for admin/index

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var tipsy = require('tipsy-browserify')($);
+require('./common/livestamp.min');
 
 $(function () {
   $('form.delete input[type=submit]').tipsy({ gravity: 'e' });
